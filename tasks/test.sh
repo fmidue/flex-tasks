a05fb77179963242a2bfcd3a005954fddcf3a367
patritzenfeld <patrick.ritzenfeld@uni-due.de>
2024-10-10 15:42:41 +0200

    change var name

--- tasks/test.sh
+++ tasks/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ $# -ne 2 ]; then
-  echo "Usage: $0 inputfile pkgdb-directory"
+  echo "Usage: $0 input_file pkgdb_directory"
   exit 1
 fi
 
@@ -13,35 +13,35 @@ fi
 base_name=$(basename "$1" | sed 's/\(.*\)\..*/\1/')
 pkg_path=$PWD/$2/pkgdb
 expect_script="$PWD/runGhci.expect"
-outputfile1="${base_name}/Global.hs"
-outputfile2="${base_name}/TaskData.hs"
-outputfile3="${base_name}/Description.hs"
-outputfile4="${base_name}/Parse.hs"
-current_output="$outputfile1"
+output_file1="${base_name}/Global.hs"
+output_file2="${base_name}/TaskData.hs"
+output_file3="${base_name}/Description.hs"
+output_file4="${base_name}/Parse.hs"
+current_output="$output_file1"
 CYAN='\033[0;36m'
 NC='\033[0m'
 
 echo -e "${CYAN}Writing .hs files...${NC}"
 
 mkdir -p "${base_name}"
-true >"$outputfile1"
-rm -f "$outputfile2" "$outputfile3" "$outputfile4"
+true >"$output_file1"
+rm -f "$output_file2" "$output_file3" "$output_file4"
 
 while IFS= read -r line || [ -n "$line" ]; do
   # Check for module separator
   if [[ "$line" =~ === ]]; then
     case $current_output in
-    "$outputfile1")
-      current_output="$outputfile2"
-      true >"$outputfile2"
+    "$output_file1")
+      current_output="$output_file2"
+      true >"$output_file2"
       ;;
-    "$outputfile2")
-      current_output="$outputfile3"
-      true >"$outputfile3"
+    "$output_file2")
+      current_output="$output_file3"
+      true >"$output_file3"
       ;;
-    "$outputfile3")
-      current_output="$outputfile4"
-      true >"$outputfile4"
+    "$output_file3")
+      current_output="$output_file4"
+      true >"$output_file4"
       ;;
     esac
     # Skip writing the line with ===
