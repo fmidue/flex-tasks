ef8cec53c2e0f2ab7039bac7626f3c0b16f78b57
patritzenfeld <patrick.ritzenfeld@uni-due.de>
2024-12-04 11:59:23 +0100

    add hidden field to checkboxes to signal no checked boxes explicitly

--- flex-tasks/src/FlexTask/Generic/Form.hs
+++ flex-tasks/src/FlexTask/Generic/Form.hs
@@ -56,10 +56,10 @@ import Data.Text            (Text, pack, unpack)
 import Yesod
 
 import FlexTask.Widgets
-  ( horizontalRadioField
+  ( checkboxField
+  , horizontalRadioField
   , joinRenders
   , renderForm
-  , verticalCheckboxesField
   )
 import FlexTask.YesodConfig (FlexForm, Handler, Rendered)
 
@@ -566,8 +566,8 @@ renderNextMultipleChoiceField pairsWith =
                                       , True
                                       , areq $
                                           case align of
-                                            Vertical -> verticalCheckboxesField
-                                            Horizontal -> checkboxesField
+                                            Vertical   -> checkboxField True
+                                            Horizontal -> checkboxField False
                                           $ withOptions opts
                                       )
       _ -> error "Incorrect naming scheme for a multi choice!"
