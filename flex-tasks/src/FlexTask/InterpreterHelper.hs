386f3aff956882edf57ba2cefec0078efc5f2954
patritzenfeld <patrick.ritzenfeld@uni-due.de>
2025-02-03 15:32:07 +0100

    adjust InterpreterHelper for type change

--- flex-tasks/src/FlexTask/InterpreterHelper.hs
+++ flex-tasks/src/FlexTask/InterpreterHelper.hs
@@ -7,7 +7,6 @@ import Control.OutputCapable.Blocks.Type
 import Control.OutputCapable.Blocks.Generic (
   ($>>=),
   )
-import Data.Either                      (fromRight)
 
 
 
@@ -15,7 +14,7 @@ type Report = ReportT Output IO
 
 
 syntaxAndSemantics
-  :: (String -> Either (LangM Report) (LangM' Report b))
+  :: (String -> LangM' Report b)
   -> (a -> FilePath -> b -> LangM Report)
   -> (a -> FilePath -> b -> Rated Report)
   -> String
@@ -25,13 +24,13 @@ syntaxAndSemantics
 syntaxAndSemantics preprocess syntax semantics input tData path  = do
   let
     parserRes = preprocess input
-    syn = either id ($>>= syntax tData path) parserRes
+    syn = parserRes $>>= syntax tData path
   synRes <- getOutputSequence syn
   if any isAbort synRes
     then
       pure (synRes,Nothing)
     else do
-      let sem = fromRight undefined parserRes $>>= semantics tData path
+      let sem = parserRes $>>= semantics tData path
       semRes <- getOutputSequenceWithRating sem
       pure (synRes, Just semRes)
 
