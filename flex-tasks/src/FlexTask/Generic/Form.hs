c0f7196db805688c5378d2dab20b8930d81100fe
patritzenfeld <patrick.ritzenfeld@uni-due.de>
2025-09-04 11:47:58 +0200

    disable type error for test

--- flex-tasks/src/FlexTask/Generic/Form.hs
+++ flex-tasks/src/FlexTask/Generic/Form.hs
@@ -12,13 +12,14 @@ module FlexTask.Generic.Form (
   ) where
 
 
-import GHC.TypeLits (TypeError, ErrorMessage(Text,(:$$:)))
+-- import GHC.TypeLits (TypeError, ErrorMessage(Text,(:$$:)))
 
 import FlexTask.Generic.FormInternal
 
-
+{-
 instance {-# Overlappable #-} TypeError (
   'Text "Formify instances for nested lists are not supported."
   ':$$: 'Text "Please use a newtype or custom datatype instead."
   ) => Formify [[a]] where
   formifyImplementation = error "unreachable"
+-}
