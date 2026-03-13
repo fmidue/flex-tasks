8e134b5a54ff4d8ef1c09e379002bc33dab46028
patritzenfeld <patrick.ritzenfeld@uni-due.de>
2026-03-13 18:38:30 +0100

    more docs for new function and implicit imports

--- flex-tasks/src/FlexTask/FormHelpers.hs
+++ flex-tasks/src/FlexTask/FormHelpers.hs
@@ -5,18 +5,34 @@ module FlexTask.FormHelpers (
   ) where
 
 
-import Yesod
+import Yesod                            (FieldSettings)
 
-import FlexTask.FormUtil
-import FlexTask.Generic.Form
-import FlexTask.YesodConfig
+import FlexTask.FormUtil                (universalLabel)
+import FlexTask.Generic.Form (
+  Alignment,
+  MultipleChoiceSelection,
+  buttons,
+  formify,
+  )
+import FlexTask.YesodConfig             (FlexForm, Rendered, Widget)
 
 
 
-labeledCheckboxes :: Alignment -> FieldSettings FlexForm -> [[Char]] -> Rendered Widget
+{- |
+A multiple choice checkbox field.
+Checkboxes are labeled with their index number and the provided (monolingual) labels.
+-}
+labeledCheckboxes
+  :: Alignment
+  -- ^ vertical or horizontal
+  -> FieldSettings FlexForm
+  -- ^ FieldSettings of the header label: attributes, label text, etc.
+  -> [String]
+  -- ^ individual option labels
+  -> Rendered Widget
 labeledCheckboxes alignment fSettings labels = formify (Nothing @MultipleChoiceSelection)
   [[buttons
       alignment
       fSettings
-      $ zipWith (\a b -> universalLabel $ show a ++ ". " ++ b) [1 :: Int ..] labels
+      $ zipWith (\a b -> universalLabel $ show a ++ ". " ++ b) [1 :: Integer] labels
   ]]
